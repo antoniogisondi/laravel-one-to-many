0c3fdbf76017a32b0389dc999d733d655f5efc42
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-24 20:41:35 +0200

    Implementation db records in create view

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -21,28 +21,37 @@
                         @endif
                         <form action="{{ route('admin.projects.store')}}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="titolo" class="form-label">Inserisci il titolo</label>
                                 <input type="text" class="form-control" id="titolo" name="titolo" placeholder="Inserisci il titolo del progetto" value="{{ old('titolo')}}">
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="descrizione" class="form-label">Inserisci descrizione</label>
                                 <textarea class="form-control" id="descrizione" name="descrizione" rows="3" placeholder="Inserisci la descrizione del progetto">{{ old('descrizione')}}</textarea>
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="img" class="form-label">Inserisci immagine</label>
                                 <input type="file" class="form-control" id="img" name="img">
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
+                                <label for="type_id" class="form-label">Seleziona la tipologia di progetto</label>
+                                <select name="type_id" id="type_id" class="form-control">
+                                    <option value="" selected>Seleziona il tipo di progetto</option>
+                                    @foreach ($types as $type)
+                                        <option value="">{{ $type->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group mb-3">
                                 <label for="inizio_progetto" class="form-label">Data di inizio</label>
                                 <input type="text" class="form-control" id="inizio_progetto" name="inizio_progetto" placeholder="Inserisci la data di inizio" value="{{ old('inizio_progetto')}}">
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="consegna_progetto" class="form-label">Data di consegna</label>
                                 <input type="text" class="form-control" id="consegna_progetto" name="consegna_progetto" placeholder="Inserisci la data di consegna" value="{{ old('consegna_progetto')}}">
                             </div>
 
-                            <div class="submit mb-3">
+                            <div class="form-group submit mb-3">
                                 <button type="submit" class="btn btn-success">Crea il tuo progetto</button>
                                 <a href="{{ route('admin.projects.index')}}" class="btn btn-primary">Torna ai progetti</a>
                             </div>
