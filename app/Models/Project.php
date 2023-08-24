d6cb210ac8a7d3de0c2aec50937478b5a70ede74
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-24 20:32:12 +0200

    Table relationship done

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['titolo','descrizione','inizio_progetto','consegna_progetto','approvato', 'non_approvato', 'img'];
+        /**
+         * Get the user associated with the Project
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\HasOne
+         */
+        public function types(): HasOne
+        {
+            return $this->belongsTo(Type::class);
+        }
 }
+
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
 
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
+
+    /**
+     * Get all of the comments for the Type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
